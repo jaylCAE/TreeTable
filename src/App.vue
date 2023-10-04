a8f90bcf831d80c511f825df2489e021f0fb607c
jaylCAE <109693400+jaylCAE@users.noreply.github.com>
2023-10-04 14:38:12 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="card">
+        <TreeTable :value="nodes">
+            <Column field="name" header="Name" expander></Column>
+            <Column field="size" header="Size"></Column>
+            <Column field="type" header="Type"></Column>
+        </TreeTable>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { NodeService } from '@/service/NodeService';
+
+onMounted(() => {
+    NodeService.getTreeTableNodes().then((data) => (nodes.value = data));
+});
+
+const nodes = ref();
+</script>
